7fffbfab5cda2f4824578b800b7059e03f69e40b
AnarRussia1219 <ft2j5fqf@duck.com>
2024-06-30 02:25:54 +0100

    configure webpack and setup

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+    entry: path.join(__dirname, "src/index.js"),
+    output: {
+        filename: "scripts.js",
+        path: path.resolve(__dirname, "src-build"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+};

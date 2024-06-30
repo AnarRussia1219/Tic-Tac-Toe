ce1c3a87aa56c9b521cc45416837f9d499ffa88f
AnarRussia1219 <ft2j5fqf@duck.com>
2024-06-30 19:12:26 +0100

    allow hte basic functionality of the game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,166 @@
 import "./styles.css";
+
+function GameBoard(player1, player2) {
+    let playing = true;
+    const getPlaying = function() {
+        return playing;
+    };
+    const setPlaying = function(val) {
+        playing = val;
+    }
+    let gameBoardArray = [
+        new GameBoardBox("", false), new GameBoardBox("", false), new GameBoardBox("", false),
+        new GameBoardBox("", false), new GameBoardBox("", false), new GameBoardBox("", false),
+        new GameBoardBox("", false), new GameBoardBox("", false), new GameBoardBox("", false),
+    ];
+
+    const getGameBoardArray = function() {
+        return gameBoardArray;
+    };
+
+    const updateGameBoardArray = function(index, to) {
+        gameBoardArray[index] = to;
+    };
+
+    let currentPlayer = player1;
+    const getCurrentPlayer = function() {
+        return currentPlayer;
+    };
+    const updateCurrentPlayer = function() {
+        if (currentPlayer === player1) {
+            currentPlayer = player2;
+        } else if (currentPlayer === player2) {
+            currentPlayer = player1;
+        };
+    };
+
+    let player1Score = 0;
+    let player2Score = 0;
+    let ties = 0;
+    const getScore = function() {
+        return {
+            player1Score: player1Score,
+            player2Score: player2Score,
+            ties: ties,
+        };
+    };
+    const updateScore = function(player, to) {
+        if (player === 1) {
+            player1Score = to;
+        } else if (player === 2) {
+            player2Score = to;
+        };
+    };
+
+    const resetGameBoardArray = function() {
+        gameBoardArray = [
+            new GameBoardBox("", false), new GameBoardBox("", false), new GameBoardBox("", false),
+            new GameBoardBox("", false), new GameBoardBox("", false), new GameBoardBox("", false),
+            new GameBoardBox("", false), new GameBoardBox("", false), new GameBoardBox("", false),
+        ];
+    };
+
+    return {
+        player1: player1,
+        player2: player2,
+        getScore, updateScore, getCurrentPlayer, updateCurrentPlayer, getGameBoardArray,
+        updateGameBoardArray, resetGameBoardArray, getPlaying, setPlaying
+    };
+};
+
+function GameBoardBox(value, selected) {
+    this.value = value;
+    this.selected = selected;
+};
+
+const Game = GameBoard("x", "o");
+const gridBoxes = document.querySelectorAll(".grid-box");
+const scoreInfoPlayer1Text = document.querySelector(".score-info-player-1-text");
+const scoreInfoPlayer1Number = document.querySelector(".score-info-player-1-number");
+const scoreInfoPlayer2Text = document.querySelector(".score-info-player-2-text");
+const scoreInfoPlayer2Number = document.querySelector(".score-info-player-2-number");
+const scoreInfoTieNumber = document.querySelector(".score-info-tie-number");
+
+updateUIInfo();
+
+function updateUIInfo() {
+    if (Game.getCurrentPlayer() === Game.player1) {
+        document.querySelector(".score-info-box:nth-child(1)").style.color = "#ffffff";
+        document.querySelector(".score-info-box:nth-child(2)").style.color = "rgba(255, 255, 255, 0.5)";
+        document.querySelector(".score-info-box:nth-child(3)").style.color = "rgba(255, 255, 255, 0.5)";
+    } else if (Game.getCurrentPlayer() === Game.player2) {
+        document.querySelector(".score-info-box:nth-child(3)").style.color = "#ffffff";
+        document.querySelector(".score-info-box:nth-child(1)").style.color = "rgba(255, 255, 255, 0.5)";
+        document.querySelector(".score-info-box:nth-child(2)").style.color = "rgba(255, 255, 255, 0.5)";
+    };
+    scoreInfoPlayer1Text.textContent = `PLAYER 1(${Game.player1})`;
+    scoreInfoPlayer1Number.textContent = String(Game.getScore().player1Score);
+
+    scoreInfoPlayer2Text.textContent = `PLAYER 2(${Game.player2})`;
+    scoreInfoPlayer2Number.textContent = String(Game.getScore().player2Score);
+
+    scoreInfoTieNumber.textContent = String(Game.getScore().ties);
+};
+
+const winningGameBoardsequences = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+gridBoxes.forEach((gridBox, index) => {
+    gridBox.addEventListener("click", function() {
+        if (!Game.getGameBoardArray()[index].selected) {
+            if (Game.getPlaying()) {
+                // console.log(1); // test
+                Game.updateGameBoardArray(index, new GameBoardBox(Game.getCurrentPlayer(), true));
+
+                // render the gameboard
+                document.querySelectorAll(".grid-box-text")[index].textContent = Game.getGameBoardArray()[index].value;
+                document.querySelectorAll(".grid-box-text")[index].style.scale = "1";
+
+                updateUIInfo();
+
+                // check if won
+                for (let i = 0; i < winningGameBoardsequences.length; i++) {
+                    const [a, b, c] = winningGameBoardsequences[i];
+                    if (Game.getGameBoardArray()[a].value && Game.getGameBoardArray()[a].value === Game.getGameBoardArray()[b].value && Game.getGameBoardArray()[a].value === Game.getGameBoardArray()[c].value) {
+                        Game.setPlaying(false);
+                        document.querySelectorAll(".grid-box-text").forEach((text, index) => {
+                            if (index !== a && index !== b && index !== c) {
+                                text.style.color = "rgba(255, 255, 255, 0.5)";
+                            };
+                        });
+                        if (Game.getCurrentPlayer() === Game.player1) {
+                            Game.updateScore(1, Number(Game.getScore().player1Score + 1));
+                        } else if (Game.getCurrentPlayer() === Game.player2) {
+                            Game.updateScore(2, Number(Game.getScore().player2Score + 1));
+                        };
+
+                        updateUIInfo();
+
+                        return; break;
+                    };
+                };
+
+                Game.updateCurrentPlayer();
+                updateUIInfo();
+            } else {
+                Game.setPlaying(true);
+                Game.resetGameBoardArray();
+                Game.updateCurrentPlayer();
+                document.querySelectorAll(".grid-box-text").forEach(text => {
+                    text.textContent = "";
+                    text.style.color = "#ffffff";
+                });
+            };
+        };
+    });
+});
